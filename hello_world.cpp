50024a6a514eb11f2d1e35524aedca002b525ee7
Strltz <145230912+Strltz@users.noreply.github.com>
2024-05-12 14:27:26 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,6 @@
 int main(int argc, char* argv[]) {
     std::cout << "Enter name" << std::endl;
     std::string name;
-    std::cin >> name; // name
+    std::cin >> name; // имя
     std::cout << "Hello world from " << name << std::endl;
 }
