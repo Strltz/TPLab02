d4392193a2ebe3003a6c2b2a36f1eab6369dbfce
Strltz <matushin_savva@mail.ru>
2024-05-12 13:27:46 +0300

    with comment commit

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,8 @@
 #include <string>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Enter name:\n";
+    std::cout << "Enter name" << std::endl;
     std::string name;
-    std::cin >> name;
+    std::cin >> name; // name
     std::cout << "Hello world from " << name << std::endl;
-    return 0;
 }
